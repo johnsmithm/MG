132cf753bfa61a328fdc7975bcd70bda2f5103f3
johnsmithm <johnsmithm@users.noreply.github.com>
2016-05-06 11:23:00 +0200

    simple template for restriction and interpolation

--- MG.h
+++ MG.h
@@ -39,7 +39,8 @@ class MG{
 			//make two for loops for red and black nodes, it is enough for the task
 			
 			double TB = 1./((nr-1)*(nr-1)), MIJ = 2*TB, LR = TB; // here we have the stencil
-
+			//todo calculate the stencil corect
+			
 			//black nodes
 			for(int i=1;i<nr-1;++i)
 				for(int j=(i%2?1:2);j<nr-1;j+=2)
@@ -53,49 +54,55 @@ class MG{
 
 		}
 
-		void downsampling(double * a, int nr){//from nr to nr/2, a is nr*nr
+		//restriction
+		void downsampling(int lev){//from nr to nr/2, a is nr*nr
 			//full weighting for restriction
 			//for
-			*a = nr;
+			int nr = (1<<(l-lev))+1;
+			double TB = 1./((nr-1)*(nr-1)), MIJ = 2*TB, LR = TB; // here we have the stencil
+
+			//black nodes
+			for(int i=1;i<nr-1;++i)
+				for(int j=1;j<nr-1;j+=1){
+					//calculate rezidual
+					double rezidialCell =  - (TB*(grids[lev][(i-1)*nr+j]+grids[lev][(i+1)*nr]+j)
+					+ LR*(grids[lev][i+nr+j+1]+grids[lev][i*nr+j-1]) + MIJ*grids[lev][i*nr+j]) + f[lev][i*nr+j];
+					
+					//from small matrix from rezidual grids[lev+1]
+					//grid[lev+1][indices2] += residialCell * somesclaler(1/2,1,1/4); todo
+				}
 		}
 
-		void interpolation(double * a, int nr){//from nr/2 to nr, (a) is (nr/2)*(nr/2)
+		void interpolation(int lev){//from nr/2 to nr, (a) is (nr/2)*(nr/2)
 			//bi-linear interpolation
-			for(int i=1;i<nr/2;++i){
-				for(int j=1;j<nr/2;++j){
-					//Todo choose corect the indices!!!
-					//spread to the neighbours
-					if(i+1 < nr/2-1)//check if we are not at the boundary
-						a[(i*2)*N+j*2] = (1/2)*a[(i)*N+j+1+nr];//nr is here offset
+			int nr = (1<<(l-lev))+1;
+			for(int i=1;i<nr-1;++i){
+				for(int j=1;j<nr-1;++j){
+					//construct each cell
 					//amd so on more 9 times, or can use the stencil and one more for loop
+					//grid[lev][indices2] += grid[lev+1][indices1] * somesclaler(1/2,1,1/4); todo
 				}
 			}
 			
 		}
 
-		//a - pointer to top-left corner of the grid
-		//nr - size of a matrix, note that the width is 2N when propragate through
-		//f - ponter to the left hand side the left hand side of size nr
-		// note 2N is the lenght of the total grid, so the second line of the grid (will be 2*N + a)
+		//level
 		void recoursionMG(int lev){
 
 			for(int i=0;i<1;i++)
 				smooth(grids[lev],(1<<(l-lev))+1,f[lev]);
 
 			return;
-			downsampling(grids[lev],(1<<(l-lev))+1);
+			downsampling(lev);
 
-			//calculate f
 
 			if(lev == l) {//use a solver
-				//*a = -(*a);
+				//todo calculate the solution
 			}else{
-				//set to zero nr/2+1 times nr/2+1 entries on (a+nr) matrix
-				// nr/2+1 is because nr is odd, and inner grid begin with 1
-				recoursionMG(l+1);//Todo choose corect the indices!!!
+				recoursionMG(l+1);
 			}
 
-			interpolation(grids[lev],(1<<(l-lev))+1);
+			interpolation(lev);
 	
 			for(int i=0;i<2;i++)
 				smooth(grids[lev],(1<<(l-lev))+1,f[lev]);
@@ -104,20 +111,21 @@ class MG{
 
 		double Lnorm(){
 			double norm = 0.;
-
+			//ToDo calculate the rezidual of the grid 
 			return norm;
 		}
 
 		double residualNorm(){
 			double norm = 0.;
-			//ToDo calculate the rezidual of the grid from the squere with the top-left corner at 'a'
+			//ToDo calculate the rezidual of the grid 
 			return norm;
 		}
 
 	public:	
 		void solve(){
 			initiate();
-
+			
+			//debug
 			cerr<<((1<<l)+1)<<"x"<<((1<<l)+1)<<"\n";
 			test_print(grids[0],((1<<l)+1));
 
@@ -129,12 +137,13 @@ class MG{
 				cout<<"residualNorm:"<<residualNorm()<<"\n";
 			}	
 
+			//debug
 			cerr<<((1<<l)+1)<<"x"<<((1<<l)+1)<<"\n";
 			test_print(grids[0],((1<<l)+1));
 		}
 
 		void print_gnuplot(){
-		//print NXN grid 
+		//print NXN grid todo
 		}
 
 		void test_print(double a[], int nr){
@@ -149,12 +158,12 @@ class MG{
 		int l,n,N;//levels, # v-cycles, # grid points including boundary
 
 
-		double **grids;// pointer to the N times 2N matrix that will be storing the grids
-		// Each grid left-top corner will be: *grids, *(grids+N), *(grids + N/2) and so on
-		// Note that the lenght of the matrix id 2N 
+		double **grids;// Vector od vector(matrix)
+		//Each grid  will be: (1^l)+1 + (1^l)+1 size and so on
+		
 
-		double ** f; // right hand side -vector of vectors N
-		//each vector for a size(the begining): *v , *(v+N), *(v+N+N/2) ans so on 
+		double ** f; // right hand side - Vector od vector(matrix)
+		//Each grid  will be: (1^l)+1 + (1^l)+1 size and so on
 
 		double pi = 3.14;
 };

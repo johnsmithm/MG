7d6642de8ba895f2cbc0d9ed78849e5dea414e32
johnsmithm <moshnoi2000@gmail.com>
2016-05-08 16:05:08 +0200

    IMPLEMENT RESTRICTION

--- MG.h
+++ MG.h
@@ -54,32 +54,75 @@ class MG{
 
 		}
 
+		bool notBoundary(int x, int nr){
+			return true;
+			if(x<nr)return false;
+			if(x >= nr*(nr-1))return false;
+			if(x%nr == 1) return false;
+			if(x%nr == nr-1)return false;
+			return true;
+			//todo check if correct
+		}
+
 		//restriction
 		void downsampling(int lev){//from nr to nr/2, a is nr*nr
 			//full weighting for restriction
 			//for
 			int nr = (1<<(l-lev))+1;
+			int nr1 = (1<<(l-lev-1))+1;
 			double TB = 1./((nr-1)*(nr-1)), MIJ = 2*TB, LR = TB; // here we have the stencil
 			
-			// todo: set f[lev+1] to 0
+			//  set f[lev+1] to 0
+			for(int i=0;i<nr1;++i)
+				for(int j=0;j<nr1;++j)
+					f[lev+1][i*nr1+j]=0;
 
 			for(int i=1;i<nr-1;++i)
 				for(int j=1;j<nr-1;j+=1){
 					//calculate rezidual
 					double rezidialCell =  - (TB*(grids[lev][(i-1)*nr+j]+grids[lev][(i+1)*nr]+j)
 					+ LR*(grids[lev][i+nr+j+1]+grids[lev][i*nr+j-1]) + MIJ*grids[lev][i*nr+j]) + f[lev][i*nr+j];
-					grids[lev][i+nr+j+1] += rezidialCell*0;
+					//cout<<i<<" j="<<j<<" r="<<rezidialCell<<" ";
 					//from small matrix from rezidual grids[lev+1]
-					//f[lev+1][indices2] += residialCell * somesclaler(1/2,1,1/4); todo
-					/*
-					check what point it is: between two vertical(horisontal,diagonal) point or the point to be get.
-					use if condition.
-					vertical((is added to two points))f[lev+1][i*(nr/2+1)+j/2] += residialCell/2; f[lev+1][i*(nr/2-1)+j/2] += residialCell/2;
-					horisontal((is added to two points))f[lev+1][i*(nr/2)+j/2+1] += residialCell/2; f[lev+1][i*(nr/2)+j/2-1] += residialCell/2;
-					diagonal(is added to four points) f[lev+1][i*(nr/2+1)+j/2+1] += residialCell/4; f[lev+1][i*(nr/2-1)+j/2+1] += residialCell/4 ...
-					the point(added to one point) - f[lev+1][i*(nr/2)+j/2] += residialCell
-					TODO - get the correct indices
-					*/
+					//f[lev+1][indices2] += residialCell * somesclaler(1/2,1,1/4);
+					if(i%2==0 && j%2==0){//the point to be restricted
+						f[lev+1][(i/2)*nr1+j/2] += rezidialCell;
+						//cout<<"the cell\n";
+					}else{
+						if(i%2==1 && j%2==1){//we add to the diagonal
+							//cout<<"diagonal"<<"\n";
+							if(notBoundary(((i+1)/2)*(nr1)+(j+1)/2,nr1))
+								f[lev+1][((i+1)/2)*(nr1)+(j+1)/2] += rezidialCell/4.;//right top							
+							if(notBoundary(((i-1)/2)*(nr1)+(j+1)/2,nr1))
+								f[lev+1][((i-1)/2)*(nr1)+(j+1)/2] += rezidialCell/4.;//right bottom
+							if(notBoundary(((i+1)/2)*(nr1)+(j-1)/2,nr1))
+								f[lev+1][((i+1)/2)*(nr1)+(j-1)/2] += rezidialCell/4.;//left top
+							if(notBoundary(((i-1)/2)*(nr1)+(j-1)/2,nr1))
+								f[lev+1][((i-1)/2)*(nr1)+(j-1)/2] += rezidialCell/4.;//right bottom
+
+						}else if(i%2==1){//we add to top and bottom
+							//cout<<"tb\n";
+							if(notBoundary(((i+1)/2)*(nr1)+(j)/2,nr1))
+								f[lev+1][((i+1)/2)*(nr1)+(j)/2] += rezidialCell/4.;// top							
+							if(notBoundary(((i-1)/2)*(nr1)+(j)/2,nr1))
+								f[lev+1][((i-1)/2)*(nr1)+(j)/2] += rezidialCell/4.;// bottom
+
+						}else{//we add to left and right		
+							//cout<<"rl\n";
+							if(notBoundary(((i)/2)*(nr1)+(j+1)/2,nr1))
+								f[lev+1][((i)/2)*(nr1)+(j+1)/2] += rezidialCell/4.;//right 							
+							if(notBoundary(((i)/2)*(nr1)+(j-1)/2,nr1))
+								f[lev+1][((i)/2)*(nr1)+(j-1)/2] += rezidialCell/4.;//right 
+						}
+					}
+				}
+
+			//set boundaries to zeor
+				for(int j=0;j<nr1;++j){
+					f[lev+1][(nr1-1)*nr1+j]=0;
+					f[lev+1][(0)*nr1+j]=0;
+					f[lev+1][(j)*nr1+0]=0;
+					f[lev+1][(j)*nr1+nr1-1]=0;					
 				}
 		}
 
@@ -102,9 +145,9 @@ class MG{
 			for(int i=0;i<1;i++)
 				smooth(grids[lev],(1<<(l-lev))+1,f[lev]);
 
-			return;
+			
 			downsampling(lev);
-
+			return;
 
 			if(lev == l) {//use a solver
 				//todo calculate the solution
@@ -130,12 +173,12 @@ class MG{
 			int nr = (1<<l) +1 ;
 			int dom = (1<<l)-1;    // Size of the domain where we calculate the norm>> internal grid points
 			int lev=0;             // To check if this gives the updated fine matrix after interpolation or the original matrix
-                        double TB = 1./((nr-1)*(nr-1)), MIJ = 2*TB, LR = TB;
+            double TB = 1./((nr-1)*(nr-1)), MIJ = 2*TB, LR = TB;
 			for ( int i=1;i< nr-1 ; i+=1)
 				{
 				for (int j=1;j<nr-1;j+=1)
 				     {
-					temp =  (TB*(grids[lev][(i-1)*nr+j]+grids[lev][(i+1)*nr]+j)
+					temp =  -(TB*(grids[lev][(i-1)*nr+j]+grids[lev][(i+1)*nr]+j)
 					+ LR*(grids[lev][i+nr+j+1]+grids[lev][i*nr+j-1]) - MIJ*grids[lev][i*nr+j]) + f[lev][i*nr+j];
 
 					residuum+=temp*temp;
@@ -155,6 +198,8 @@ class MG{
 			cerr<<((1<<l)+1)<<"x"<<((1<<l)+1)<<"\n";
 			test_print(grids[0],((1<<l)+1));
 
+		
+
 			//perform MG n times
 			for(int i=0;i<1;++i){
 				recoursionMG(0);
@@ -166,6 +211,9 @@ class MG{
 			//debug
 			cerr<<((1<<l)+1)<<"x"<<((1<<l)+1)<<"\n";
 			test_print(grids[0],((1<<l)+1));
+
+			cerr<<((1<<(l-1))+1)<<"x"<<((1<<(l-1))+1)<<"\n";
+			test_print(f[1],((1<<(l-1))+1));
 		}
 
 		void print_gnuplot(){
